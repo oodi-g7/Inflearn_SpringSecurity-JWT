6fa5eb2507c107ec28daaa83f4af7a32b1d80678
oodi-g7 <oodig7@gmail.com>
2023-08-17 23:04:48 +0900

    section23. jwt임시토큰만들어서 테스트해보기

--- security_jwt/src/main/java/com/cos/jwt/filter/MyFilter1.java
+++ security_jwt/src/main/java/com/cos/jwt/filter/MyFilter1.java
@@ -13,8 +13,8 @@ public class MyFilter1 implements Filter{
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+		
 		System.out.println("필터1");
 		chain.doFilter(request, response);
 	}
-
 }
